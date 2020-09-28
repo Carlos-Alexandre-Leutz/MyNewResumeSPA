cc02f8b1a74347bea6167380bcd4a1cafa21c9d2
Carlos Alexandre Leutz <54986289+Carlos-Alexandre-Leutz@users.noreply.github.com>
2020-09-28 08:48:07 -0300

    alexandre

--- app.js
+++ app.js
@@ -3,25 +3,34 @@ var app = angular.module("App", ["ngRoute"]);
 app.config(function($routeProvider) {
     $routeProvider
         .when("/", {
-            templateUrl: "template/home.html"
+            templateUrl: "template/home.html",
+            controller: 'HomeCtrl',
+
         })
         .when("/pagina1", {
             templateUrl: "template/p1.html"
+
         })
         .when("/pagina2", {
             templateUrl: "template/p2.html"
+
         })
-        .when("/pagina3", {
+
+    .when("/pagina3", {
             templateUrl: "template/p3.html"
+
         })
         .when("/pagina4", {
             templateUrl: "template/p4.html"
+
         })
         .when("/pagina5", {
             templateUrl: "template/p5.html"
+
         })
         .when("/pagina6", {
             templateUrl: "template/p6.html"
+
         })
         .otherwise({ redirectTo: '/' })
 })
